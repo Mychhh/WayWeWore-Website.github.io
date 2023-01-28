b1f877a8bb1ae463a636475df1f362861cf1e9a1
Michael <michaeldrpabaya@gmail.com>
2023-01-28 16:52:42 +0800

    animation on mobile menu slider

--- JS/javascript.js
+++ JS/javascript.js
@@ -3,10 +3,18 @@ var a;
 function show_hide(){
 
     if(a == 1){
+        // removes the slider menu
         document.getElementById("menu-container-mobile").style.display="none";
+        //animation to show menu to the right
+        document.getElementById("hamburger-menu").style.transform = "translate(0%)";
+        document.getElementById("hamburger-menu").style.transitionDuration = "1s";
         return a = 0;
     }else{
+        // shows the slider menu
         document.getElementById("menu-container-mobile").style.display="flex";
+        //animation to hide menu to the left
+        document.getElementById("hamburger-menu").style.transform = "translate(-300%)";
+        document.getElementById("hamburger-menu").style.transitionDuration = "1s";
         return a = 1;
     }
     
