646450b7ef39bf80a313372da8c8ee7751cf9162
Michael <michaeldrpabaya@gmail.com>
2023-01-29 15:46:17 +0800

    slider

--- JS/nav-script.js
+++ JS/nav-script.js
@@ -6,15 +6,15 @@ function show_hide(){
         // removes the slider menu
         document.getElementById("menu-container-mobile").style.display="none";
         //animation to show menu to the right
-        document.getElementById("hamburger-menu").style.transform = "translate(0%)";
-        document.getElementById("hamburger-menu").style.transitionDuration = "1s";
+        document.getElementById("nav__hamburger-menu").style.transform = "translate(0%)";
+        document.getElementById("nav__hamburger-menu").style.transitionDuration = "1s";
         return a = 0;
     }else{
         // shows the slider menu
         document.getElementById("menu-container-mobile").style.display="flex";
         //animation to hide menu to the left
-        document.getElementById("hamburger-menu").style.transform = "translate(-300%)";
-        document.getElementById("hamburger-menu").style.transitionDuration = "1s";
+        document.getElementById("nav__hamburger-menu").style.transform = "translate(-300%)";
+        document.getElementById("nav__hamburger-menu").style.transitionDuration = "1s";
         return a = 1;
     }
     
