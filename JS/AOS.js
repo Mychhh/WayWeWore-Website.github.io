51cd557775574d9e014c5c111c4377c8c186e0ef
Michael <michaeldrpabaya@gmail.com>
2023-02-04 11:14:54 +0800

    animation on slider model and gallery

---
+++ JS/AOS.js
@@ -0,0 +1,35 @@
+let animationOnScroll = document.querySelectorAll(".AOS");
+
+window.addEventListener('scroll', throttle(scanElements, 1000))
+
+function scanElements(){
+    animationOnScroll.forEach(animationonscroll => {
+        if(isVisible(animationonscroll)){
+            animationonscroll.classList.add('active');
+        }else{
+            animationonscroll.classList.remove('active');
+        }
+    })
+}
+
+function isVisible(element){
+    const elementDiv = element.getBoundingClientRect();
+    let distanceFromTop = -50;
+    console.log(elementDiv.top - window.innerHeight);
+    return elementDiv.top - window.innerHeight < distanceFromTop ? true : false;
+}
+
+function throttle(fn, delay){
+    let lastCall = 0
+    return(...args) => {
+        let context = this;
+        let current = new Date().getTime();
+
+        if(current - lastCall < delay){
+            return
+        }
+
+        lastCall = current;
+        return fn.apply(context, ...args)
+    }
+}
